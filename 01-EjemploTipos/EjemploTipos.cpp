9911ae31203c75a30c7a662949796b643b6fd9f7
effy <effy@desktop>
2022-05-18 21:57:18 -0300

    Update 18/05/22

--- 01-EjemploTipos/EjemploTipos.cpp
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -9,19 +9,42 @@
 #include <cassert>
 
 int main() {
+	// Definimos cada variable como una constante para que no varíe su valor
+	// mientras corre nuestro programa.
 	const bool booleano = true;
-	const char caracter = 'A';
 	const unsigned int enteroSinSigno = 38292;
-	const int entero = -2932;
-	const double decimal = 293.92;
+	const char caracter = 'J';
+	const double decimal = -293.92;
+	// El inicializar 'entero' igual a decimal nos dará el valor de decimal
+	// redondeado hacia abajo. Es decir, sin su mantisa.
+	const int entero = decimal;
 	const std::string cadenaDeCaracteres = "Prueba";
 
+	// Simplemente pasamos la variable ya que al tener un valor verdadero
+	// y ser constante este no debería variar.
 	assert(booleano);
-	assert(caracter == 'A');
-	assert(enteroSinSigno >= 0);
-	assert(entero < 0);
-	assert(decimal > 293 && decimal < 294);
-	assert(!cadenaDeCaracteres.compare("Prueba"));
 
-	std::cout << typeid(booleano).name();
+	// Verificamos si la variable es igual o mayor a cero, ya que no puede
+	// (o debería, mejor dicho) tener un valor menor a 0.
+	assert(enteroSinSigno >= 0);
+
+	// Siendo que los chars tienen un valor númerico entre 0 y 256, podemos 
+	// verificar si la variable tiene un valor entre dichos números
+	assert(caracter >= 0 && caracter <= 255);
+	
+	// Asumiendo que decimal tiene una mantisa, de querer restar decimal a entero
+	// el valor total debería ser siempre distinto a 0.
+	// Este chequeo rinde para ambas variables entero y decimal.
+	assert(entero - decimal != 0);
+
+	// Para el string usamos una de las funciones integradas del objeto para
+	// comparar su valor con el indicado previamente.
+	assert(!cadenaDeCaracteres.compare("Prueba"));
+	
+	// Como extra, para variar un poco y divertirse, podemos verificar si el 
+	// tipo de dato de la variable 'caracter' es un char tomando el valor devuelto 
+	// por typeid (un puntero a un const char), casteando su valor como un char, 
+	// y compararlo con el valor que debería devolver si el tipo es un char.
+	assert((char)*typeid(caracter).name() == 'c');
+	
 }
--- 01-EjemploTipos/README.md
+++ 01-EjemploTipos/README.md
@@ -1,7 +1,13 @@
 # Ejercicio 01: Valores y Operaciones con Tipos de Datos
 ---
-- **Problema:** 
-    - Entradas:
-    - Proceso: 
-    - Salidas:
-- **Solución propuesta:**
+- **Problema:** Diseñar un programa en C++ que ejemplifique con pruebas la
+aplicación de los tipos de datos vistos en clase. 
+En este caso, utilizaremos el macro visto en clase, assert(), para verificar
+distintos tipos de datos y sus valores esperados.
+
+- **Solución propuesta:** Se propone definir una serie de variables con un tipo 
+de dato, para luego ejecutar el macro assert() por cada una de ellas, verificando
+su tipo.
+De no coincidir el tipo o su valor con el esperado, el programa terminará abruptamente
+y devolverá un error indicando la expresión que devolvió un valor falso dentro de
+assert().
